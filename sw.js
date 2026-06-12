c37714354f24abd5176b7d01786627a1f6763837
apl0710 <adrielle18silva.19@gmail.com>
2026-06-12 15:44:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'rrp-erp-v1';
+
+// O que ele vai salvar no celular para abrir rápido
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo_apl.png',
+  './logoapp.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Se já tem no celular, devolve rápido. Se não, baixa da net.
+        return response || fetch(event.request);
+      })
+  );
+});
